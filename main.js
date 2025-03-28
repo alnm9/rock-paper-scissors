e03e52a5e173f5008ac2ab5b109cfa66cd38ef61
Alina M <189365964+alnm9@users.noreply.github.com>
2025-03-28 14:31:50 +0100

    Update player buttons to announce a winner once one of the scores reaches 5 points

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
-const container = document.querySelector("div");
+const container = document.querySelector("#container");
+// const playBtn = document.querySelector("#playGame");
 
+// playBtn.addEventListener("click", () => {
+//     alert("The game has begun!");
+//     playGame();
+// });
 
+// function playGame() {
+
+let humanScore = 0;
+let computerScore = 0;
 
 rockBtn.addEventListener("click", () => {
     choice = "Rock";
     playRound(choice, getComputerChoice());
+    checkScore();
 })
+
 paperBtn.addEventListener("click", () => {
     choice = "Paper";
     playRound(choice, getComputerChoice());
+    checkScore();
 })
+
 scissorsBtn.addEventListener("click", () => {
     choice = "Scissors";
     playRound(choice, getComputerChoice());
+    checkScore();
 })
 
-const getComputerChoice = () => {
-    let choice = Math.floor(Math.random() * 3) + 1;
-    switch (choice) {
-        case 1:
-            return "Rock";
-            break;
-        case 2:
-            return "Paper";
-            break;
-        case 3:
-            return "Scissors";
-            break;
-    }
-}
-
-
 function playRound(humanChoice, computerChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
+
     console.log(`Human:${humanChoice}\nComputer:${computerChoice}`)
 
     if (humanChoice == "Rock" && computerChoice == "Paper") {
@@ -65,52 +62,35 @@ function playRound(humanChoice, computerChoice) {
 }
 
 
-
-
-// function playGame() {
-
-//     let humanScore = 0;
-//     let computerScore = 0;
-
-//     function playRound(humanChoice, computerChoice) {
-//         if (humanChoice == "Rock" && computerChoice == "Paper") {
-//             console.log("You lose, paper beats rock!");
-//             computerScore++;
-//         } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-//             console.log("You win, rock beats scissors!");
-//             humanScore++;
-//         } else if (humanChoice == "Paper" && computerChoice == "Rock") {
-//             console.log("You win, paper beats rock!");
-//             humanScore++;
-//         } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-//             console.log("You lose, scissors beats paper!");
-//             computerScore++;
-//         } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-//             console.log("You lose, rock beats scissors!");
-//             computerScore++;
-//         } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-//             console.log("You win, scissors beats paper!");
-//             humanScore++;
-//         } else if (humanChoice == computerChoice) {
-//             console.log("No points!")
-//         }
-
-//     }
-
-//     // for (i = 0; i < 5; i++) {
-//     //     const humanSelection = getHumanChoice();
-//     //     const computerSelection = getComputerChoice();
-//     //     playRound(humanSelection, computerSelection);
-//     //     console.log(`Human:${humanSelection}\nComputer:${computerSelection}`)
-//     //     console.log(`Human: ${humanScore}\nComputer:${computerScore}`);
-//     // }
-
-//     if (humanScore > computerScore) {
-//         console.log("You win!");
-//     } else {
-//         console.log("You lose!");
-//     }
 // }
+function checkScore() {
+    if (humanScore == 5) {
+        alert("Congratulations! You won!");
+        humanScore = 0;
+        computerScore = 0;
+
+    } else if (computerScore == 5) {
+        alert("Better luck next time!");
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
 
 
-// playGame()
+
+const getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    switch (choice) {
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+            break;
+    }
+}
+
+
